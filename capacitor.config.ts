16bab0fc5fe9dbbe4e6e8dd63833a5f07a162bfc
shyam <shyam1@live.fr>
2024-10-22 16:09:08 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Projet_Angular_Ionic_Bateau_Thibault',
+  webDir: 'www'
+};
+
+export default config;
