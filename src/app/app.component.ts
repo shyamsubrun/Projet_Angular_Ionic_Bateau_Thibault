2de93add1707bd21bad330799d0bde4be7882264
shyam <shyam1@live.fr>
2024-10-23 11:07:46 +0200

    parametre v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader,IonToolbar,IonTitle,IonButtons,IonIcon,IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonHeader,IonToolbar,IonTitle,RouterModule,IonButtons,IonIcon,IonApp, IonRouterOutlet],
+  imports: [IonHeader,IonToolbar,IonTitle,RouterModule,IonButtons,IonIcon,IonApp, IonRouterOutlet, HttpClientModule],
 })
 export class AppComponent {
   constructor() {}
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,15 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
   ],
 });
