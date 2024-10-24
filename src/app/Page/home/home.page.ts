6f35784068810aa16232da42389b618d22ff205d
ImEtienne <graddykibongani@gmail.com>
2024-10-24 11:36:34 +0200

    tollbar update++

--- src/app/Page/home/home.page.ts
+++ src/app/Page/home/home.page.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonIcon,IonList,IonToggle,IonItem,IonLabel,IonSelect,IonSelectOption,IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFooter } from '@ionic/angular/standalone';
+import { IonIcon,IonList,IonToggle,IonItem,IonLabel,IonSelect,IonSelectOption,IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFooter, IonButtons, IonBadge } from '@ionic/angular/standalone';
 import {RouterLink} from '@angular/router';
 import { TabsComponent } from 'src/app/tabs/tabs.component';  
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common'; 
 import { RouterModule } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-home',
@@ -15,8 +13,30 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['home.page.scss'],
   standalone: true,
 
-  imports: [RouterModule,CommonModule,IonFooter, TabsComponent,RouterLink,IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonList, IonToggle,
-    IonItem, IonLabel, IonSelect, IonSelectOption, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonBadge,
+    IonButtons,
+    RouterModule,
+    CommonModule,
+    IonFooter,
+    TabsComponent,
+    RouterLink,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonIcon,
+    IonList,
+    IonToggle,
+    IonItem,
+    IonLabel,
+    IonSelect,
+    IonSelectOption,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent],
 })
 export class HomePage {
   totalItems: number = 0; 
