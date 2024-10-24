6ee12d21755baa704add92ff514fcc879b5b64db
shyam <shyam1@live.fr>
2024-10-24 11:08:16 +0200

    ajout des photo

--- src/app/Page/home/home.page.ts
+++ src/app/Page/home/home.page.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonIcon,IonList,IonToggle,IonItem,IonLabel,IonSelect,IonSelectOption,IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFooter } from '@ionic/angular/standalone';
-import {RouterLink} from '@angular/router';
+import { IonIcon, IonList, IonToggle, IonItem, IonLabel, IonSelect, IonSelectOption, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFooter } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 import { TabsComponent } from 'src/app/components/tabs/tabs.component';  
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common'; // Importer CommonModule
@@ -11,20 +11,24 @@ import { CommonModule } from '@angular/common'; // Importer CommonModule
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-
-  imports: [CommonModule,IonFooter, TabsComponent,RouterLink,IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonList, IonToggle,
-    IonItem, IonLabel, IonSelect, IonSelectOption, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    CommonModule, IonFooter, TabsComponent, RouterLink,
+     IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonList,
+      IonToggle, IonItem, IonLabel, IonSelect, IonSelectOption, IonHeader, IonToolbar,
+       IonTitle, IonContent],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   totalItems: number = 0; 
 
-  constructor(private router: Router,private cartService: CartService) { }
-  
-  onGoToListAll() {
-    console.log('TEST FROMonGoToListAll');
-    this.router.navigate(['/list-all']);
+  constructor(private router: Router, private cartService: CartService) { }
+
+  ngOnInit() {
     this.cartService.totalItems$.subscribe(count => {
       this.totalItems = count;
     });
+  }
+
+  onGoToListAll() {
+    this.router.navigate(['/list-all']);
   }
 }
