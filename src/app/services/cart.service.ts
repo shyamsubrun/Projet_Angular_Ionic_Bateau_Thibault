a6c9ea13f42685866dc410158a1c2ca63576ed7e
shyam <shyam1@live.fr>
2024-10-24 09:32:26 +0200

    panier fonctionnel local storage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-
 interface CartItem {
   id: number;
   name: string;
@@ -13,12 +12,15 @@ interface CartItem {
   providedIn: 'root'
 })
 export class CartService {
-  private cartItems: CartItem[] = [];
+  private cartItems: CartItem[] = this.loadCart(); // Charger les articles au démarrage
   private cartItemsSubject = new BehaviorSubject<CartItem[]>(this.cartItems);
 
-  private items: any[] = [];
   private totalItemsSubject = new BehaviorSubject<number>(0);
   totalItems$ = this.totalItemsSubject.asObservable();
+
+  constructor() {
+    this.updateTotalItems(); // Mettre à jour le nombre total d'articles au démarrage
+  }
 
   addToCart(product: any) {
     const existingItem = this.cartItems.find(item => item.id === product.id);
@@ -28,6 +30,8 @@ export class CartService {
       this.cartItems.push({ ...product, quantity: 1 });
     }
     this.cartItemsSubject.next(this.cartItems);
+    this.updateTotalItems();
+    this.saveCart(); // Sauvegarder le panier après chaque ajout
   }
 
   removeFromCart(productId: number) {
@@ -40,21 +44,36 @@ export class CartService {
         this.cartItems.splice(itemIndex, 1); // Retire l'élément si la quantité atteint 0
       }
       this.cartItemsSubject.next(this.cartItems);
+      this.updateTotalItems();
+      this.saveCart(); // Sauvegarder le panier après chaque retrait
     }
   }
+
   getTotalItems(): number {
-    return this.cartItems.length; // Renvoie le nombre total d'articles
+    return this.cartItems.reduce((total, item) => total + item.quantity, 0);
   }
 
   getCartItems() {
     return this.cartItemsSubject.asObservable();
   }
+
   private updateTotalItems() {
     this.totalItemsSubject.next(this.getTotalItems());
   }
 
   clearCart() {
-    this.items = []; // Vider le panier
+    this.cartItems = []; // Vider le panier
+    this.cartItemsSubject.next(this.cartItems);
     this.updateTotalItems(); // Mettre à jour le nombre total d'articles
+    this.saveCart(); // Sauvegarder le panier vide
+  }
+
+  private saveCart(): void {
+    localStorage.setItem('cart', JSON.stringify(this.cartItems));
+  }
+
+  private loadCart(): CartItem[] {
+    const storedCart = localStorage.getItem('cart');
+    return storedCart ? JSON.parse(storedCart) : [];
   }
 }
